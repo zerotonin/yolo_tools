be1425105debb0e3e6c08e843ea3ec1c328cef46
bgeurten <bart.geurten@otago.ac.nz>
2024-05-02 11:50:12 +1200

    adding and laoding presets

--- PrepareMetaData4Yolo2SQL.py
+++ PrepareMetaData4Yolo2SQL.py
@@ -8,55 +8,53 @@ from database.FlyChoiceDatabase import DatabaseHandler
 from AnalysisFileManager.PresetManager import PresetManager
 from AnalysisFileManager.AnalysisFileManager import AnalysisFileManager
 
-def clear_screen():
-    """Clears the terminal screen for better readability."""
-    os.system('cls' if os.name == 'nt' else 'clear')
 
-def get_arena_configuration():
-    """
-    Prompts the user via CLI to input the number of arenas and their arrangement in rows and columns.
-    
-    Returns:
-        tuple: A tuple containing three integers: 
-               1. The total number of arenas.
-               2. The number of rows.
-               3. The number of columns.
-    
-    Raises:
-        ValueError: If the input values are not integers or if the product of rows and columns
-                    does not equal the total number of arenas.
-    """
-    print("Please enter the details for arena configuration.")
+class ExperimentSetupManager:
+    def __init__(self, base_output_path, db_file_path, video_file_path, python_env_path):
+        self.file_manager = AnalysisFileManager()
+        self.file_manager.setup_experiment_paths(base_output_path,db_file_path, video_file_path, python_env_path)
+        self.db_handler = DatabaseHandler(f'sqlite:///{db_file_path}')
+        self.experiment_manager = ExperimentManager(self.db_handler)
+        self.arena_manager = ArenaManager(self.db_handler)
+        self.fly_manager = FlyManager(self.db_handler)
+        self.fly_distribution_manager = FlyDistributionManager(self.db_handler, {})
+        self.stimulus_manager = StimulusManager(self.db_handler)
+        self.preset_manager = PresetManager()
 
-    try:
-        total_arenas = int(input("Enter the total number of arenas in the experiment: "))
-        rows = int(input("Enter the number of rows of arenas: "))
-        columns = int(input("Enter the number of columns of arenas: "))
+    def clear_screen(self):
+        """ Clears the terminal screen for better readability. """
+        os.system('cls' if os.name == 'nt' else 'clear')
 
-        # Validate that rows and columns correctly represent the total number of arenas
-        if rows * columns != total_arenas:
-            raise ValueError("The product of rows and columns must equal the total number of arenas.")
+    def get_arena_configuration(self):
+        """ Prompts user to input number of arenas and their arrangement. """
+        self.clear_screen()
+        print("Please enter the details for arena configuration.")
+        try:
+            total_arenas = int(input("Enter the total number of arenas in the experiment: "))
+            rows = int(input("Enter the number of rows of arenas: "))
+            columns = int(input("Enter the number of columns of arenas: "))
+            if rows * columns != total_arenas:
+                raise ValueError("The product of rows and columns must equal the total number of arenas.")
+        except ValueError as e:
+            print(f"Invalid input: {e}")
+            return self.get_arena_configuration()
+        return {'arena_num': total_arenas, 'arena_rows': rows, 'arena_cols': columns}
 
-    except ValueError as e:
-        print(f"Invalid input: {e}")
-        return get_arena_configuration()  # Recursively call the function again if there's an error
+    def check_loadable_preset(self, file_type):
+        """ Checks if a preset can be loaded based on the file type. """
+        return file_type in list(self.file_manager.file_dict.keys())
 
-    return {'arena_num' : total_arenas, 'arena_rows' : rows, 'arena_cols' : columns}
+    def save_preset(self, file_type, data):
+        """ Saves a preset for a given file type. """
+        preset_path = self.file_manager.folder_dict['presets']
+        new_filename = os.path.join(preset_path, file_type + '.json')
+        self.preset_manager.save_file(data, new_filename)
+        self.file_manager.file_dict[file_type] = new_filename
 
-
-def check_loadable_preset(file_manager,file_type):
-    return file_type in list(file_manager.file_dict.keys())
-
-def save_preset(file_manager,preset_file_manager,file_type,data):
-    preset_path = file_manager.folder_dict['presets']
-    new_filename = os.path.join(preset_path,file_type+'.json')
-    preset_file_manager.save_file(data,new_filename)
-    file_manager.file_dict[file_type] = new_filename
-    
+    def load_preset(self, file_type):
+        return self.preset_manager.load_json(self.file_manager.file_dict[file_type])
 
 
-
-file_manager = AnalysisFileManager()
 
 
 # file and folder paths
@@ -64,45 +62,41 @@ db_file_position = '/home/geuba03p/PyProjects/yolo_tools/fly_choice.db'
 video_file_position = '/home/geuba03p/food_example_video/original/2024_03_28__16-19-28.mp4'
 base_output_path = '/home/geuba03p/food_example_video/output'
 python_interp = '/home/geuba03p/miniconda3/envs/yolov8/bin/python'
-file_manager.setup_experiment_paths(base_output_path,db_file_position,video_file_position,python_interp)
 
+experiment = ExperimentSetupManager(base_output_path,db_file_position,video_file_position,python_interp)
 
-
-# Instantiate database handler
-db_handler = DatabaseHandler(f'sqlite:///{db_file_position}')
-
-# Initialise meta data  managers
-experiment_manager = ExperimentManager(db_handler)
-arena_manager = ArenaManager(db_handler)
-fly_manager = FlyManager(db_handler)
-fly_distribution_manager = FlyDistributionManager(db_handler,{})
-stimulus_manager = StimulusManager(db_handler)
-preset_file_manager = PresetManager()
-        
-
-if  check_loadable_preset(file_manager,'arena_layout'):
-    arena_layout = preset_file_manager.load_json(file_manager.file_dict['arena_layout'])
+if  experiment.check_loadable_preset('arena_layout'):
+    arena_layout = experiment.load_preset('arena_layout')
+    arena_info = experiment.load_preset('arena_info')
 else:
+    experiment.clear_screen()
+    arena_info =  experiment.get_arena_configuration()
+    print(f"Configuration: {arena_info['arena_num']} arenas arranged in {arena_info['arena_rows']} rows and {arena_info['arena_cols']} columns.")
+    arena_layout =  experiment.arena_manager.enter_arenas_for_experiment(arena_info['arena_num'], arena_info['arena_rows'], arena_info['arena_cols'])
+    experiment.save_preset('arena_layout',arena_layout)
+    experiment.save_preset('arena_info',arena_info)
 
-    # Get all info for posthoc run
-    clear_screen()
-    arena_layout = get_arena_configuration()
-    print(f"Configuration: {arena_layout['arena_num']} arenas arranged in {arena_layout['arena_rows']} rows and {arena_layout['arena_cols']} columns.")
-    arena_layout = arena_manager.enter_arenas_for_experiment(arena_layout['arena_num'], arena_layout['arena_rows'], arena_layout['arena_cols'])
-    save_preset(file_manager,preset_file_manager,'arena_layout',arena_layout)
 
-
-if  check_loadable_preset(file_manager,'arena_layout'):
-    arena_layout = preset_file_manager.load_json(file_manager.file_dict['arena_layout'])
-
-clear_screen()
-
-stim_layout = stimulus_manager.enter_stimuli_for_experiment(arena_layout['arena_num'], arena_layout['arena_rows'], arena_layout['arena_cols'])
-
+if  experiment.check_loadable_preset('stim_layout'):
+    stim_layout = experiment.load_preset('stim_layout')
+else:
+    experiment.clear_screen()
+    stim_layout =  experiment.stimulus_manager.enter_stimuli_for_experiment(arena_info['arena_num'], arena_info['arena_rows'], arena_info['arena_cols'])
+    experiment.save_preset('stim_layout',stim_layout)
 
 
 
-experiment_preset = preset_file_manager.load_dialogue('whole experiment',('*.csv', '*.*'))
+if  experiment.check_loadable_preset('fly_layout'):
+    fly_layout = experiment.load_preset('fly_layout')
+else:
+    flies = experiment.fly_manager.enter_flies_for_experiment()
+    experiment.fly_distribution_manager.enter_flies_for_experiment(arena_info['arena_num'])
+    fly_layout = experiment.fly_distribution_manager.distribute_flies(arena_info['arena_rows'], arena_info['arena_cols'])  # Example layout with rows and cols
+    experiment.fly_distribution_manager.show_arena_assignments()
+
+
+
+
 
 # Get automated info from posthoc run
 
@@ -115,9 +109,4 @@ video_info = video_info_extractor.get_video_info()
 experiment_info = experiment_manager.manage_experiments()
 
 # Get arena layout and other configurations
-flies = fly_manager.enter_flies_for_experiment()
-fly_distribution_manager.enter_flies_for_experiment(total_arenas)
-arenas = fly_distribution_manager.distribute_flies(9, 6)  # Example layout with rows and cols
-fly_distribution_manager.show_arena_assignments()
-
 
