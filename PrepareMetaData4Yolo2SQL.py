6d51778d7b00bef5ecc75d72da4eedbf995f5df6
bgeurten <bart.geurten@otago.ac.nz>
2024-05-02 12:55:22 +1200

    made ageneral preset loader, now working to make this if clauses part of the class

--- PrepareMetaData4Yolo2SQL.py
+++ PrepareMetaData4Yolo2SQL.py
@@ -20,6 +20,7 @@ class ExperimentSetupManager:
         self.fly_distribution_manager = FlyDistributionManager(self.db_handler, {})
         self.stimulus_manager = StimulusManager(self.db_handler)
         self.preset_manager = PresetManager()
+        self.load_existing_presets()
 
     def clear_screen(self):
         """ Clears the terminal screen for better readability. """
@@ -53,6 +54,26 @@ class ExperimentSetupManager:
 
     def load_preset(self, file_type):
         return self.preset_manager.load_json(self.file_manager.file_dict[file_type])
+    
+    def load_existing_presets(self):
+        """
+        Scans the preset folder for any existing files and updates the file_dict with these files.
+        Uses the basename of each file as the key and the full path as the value.
+        """
+        preset_folder = self.file_manager.folder_dict['presets']
+        try:
+            # List all files in the preset folder
+            for filename in os.listdir(preset_folder):
+                file_path = os.path.join(preset_folder, filename)
+                if os.path.isfile(file_path):
+                    # Use the file's basename without extension as the key
+                    key = os.path.splitext(filename)[0]
+                    self.file_manager.file_dict[key] = file_path
+                    print(f"Preset Found: {key} -> {file_path}")
+        except FileNotFoundError:
+            print("Preset folder not found.")
+        except Exception as e:
+            print(f"An error occurred: {e}")
 
 
 
@@ -64,6 +85,13 @@ base_output_path = '/home/geuba03p/food_example_video/output'
 python_interp = '/home/geuba03p/miniconda3/envs/yolov8/bin/python'
 
 experiment = ExperimentSetupManager(base_output_path,db_file_position,video_file_position,python_interp)
+
+if experiment.check_loadable_preset('experiment_info'):
+    experiment_info = experiment.load_preset('experiment_info')
+else:
+    # Start the process
+    experiment_info = experiment.experiment_manager.manage_experiments()
+    experiment.save_preset('experiment_info',experiment_info)
 
 if  experiment.check_loadable_preset('arena_layout'):
     arena_layout = experiment.load_preset('arena_layout')
@@ -86,13 +114,21 @@ else:
 
 
 
+if  experiment.check_loadable_preset('flies'):
+    flies = experiment.load_preset('fly_layout')
+else:
+    flies = experiment.fly_manager.enter_flies_for_experiment()
+    experiment.save_preset('flies',flies)
+
+    
 if  experiment.check_loadable_preset('fly_layout'):
     fly_layout = experiment.load_preset('fly_layout')
 else:
-    flies = experiment.fly_manager.enter_flies_for_experiment()
+    experiment.fly_distribution_manager.fly_data = flies
     experiment.fly_distribution_manager.enter_flies_for_experiment(arena_info['arena_num'])
     fly_layout = experiment.fly_distribution_manager.distribute_flies(arena_info['arena_rows'], arena_info['arena_cols'])  # Example layout with rows and cols
     experiment.fly_distribution_manager.show_arena_assignments()
+    experiment.save_preset('fly_layout',fly_layout)
 
 
 
@@ -105,8 +141,6 @@ video_info = video_info_extractor.get_video_info()
 
 
 
-# Start the process
-experiment_info = experiment_manager.manage_experiments()
 
 # Get arena layout and other configurations
 
