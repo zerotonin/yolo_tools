acd59da5ef9e87b1bc6ce0c4e189cad36e26d617
bgeurten <bart.geurten@otago.ac.nz>
2024-05-10 16:32:06 +1200

    first step done

--- yolo_tools/workflow/SlurmJobManager.py
+++ yolo_tools/workflow/SlurmJobManager.py
@@ -85,28 +85,27 @@ class SlurmJobManager:
         script_parameters['ntasks_per_node'] = ntasks
 
         self.create_slurm_script(script_parameters)
+        return script_parameters['filename']
 
 
 
-    def manage_workflow(self, initial_input, num_splits, split_script, tracking_script, analysis_script, final_script):
+    def manage_workflow(self, num_splits):
         """
         Manages the full workflow of splitting, tracking, analyzing, and compiling results.
         """
         # Step 1: Create and submit the split job
-        split_output = f"{self.file_base_dir}/split_output"
-        self.create_slurm_script('split_job.sh', split_script, initial_input, split_output)
-        split_job_id = self.submit_job('split_job.sh')
+        split_script_position = self.create_slurm_script()
+        #split_job_id = self.submit_job(split_script_position)
+        split_job_id = 666
 
         # Step 2: Submit tracking and analysis jobs
         analysis_jobs = []
         for i in range(num_splits):
             # Assume each split file is named uniquely
-            input_file = f"{split_output}_part_{i}.mov"
-            track_output = f"{self.file_base_dir}/track_output_{i}.dat"
-            self.create_slurm_script(f'track_job_{i}.sh', tracking_script, input_file, track_output)
-            track_job_id = self.submit_job(f'track_job_{i}.sh', dependency_id=split_job_id)
-            
-            analysis_output = f"{self.file_base_dir}/analysis_output_{i}.dat"
+            self.create_slurm_script()
+            #track_job_id = self.submit_job(f'track_job_{i}.sh', dependency_id=split_job_id)
+            track_job_id = i+100
+            analysis_output = f"{self.file_base_dir}/slurm_scripts/analysis_output_{i}.dat"
             self.create_slurm_script(f'analysis_job_{i}.sh', analysis_script, track_output, analysis_output)
             analysis_job_id = self.submit_job(f'analysis_job_{i}.sh', dependency_id=track_job_id)
             analysis_jobs.append(analysis_job_id)
@@ -117,45 +116,3 @@ class SlurmJobManager:
         self.submit_job('final_job.sh', dependency_id=all_dependencies)
 
 
-
-# Usage of the class
-if __name__ == "__main__":
-    self = SlurmJobManager('/home/geuba03p/food_example_video/output','/home/geuba03p/PyProjects/yolo_tools','/home/geuba03p/miniconda3/envs/yolov8/bin/python')
-    script_parameters = dict()
-
-    input_file_position = f'/home/geuba03p/food_example_video/original/2024_03_28__16-19-28.mp4'
-    self.create_video_splitting_slurm_script(input_file_position)
-
-    """
-
-    partition = 'aoraki'
-    input_file_position = f'/home/geuba03p/food_example_video/original/2024_03_28__16-19-28.mp4'
-    slurm_filename      = f'{self.file_base_dir}/slurm_scripts/split_video.sh'
-    video_output_folder = f'{self.file_base_dir}/preprocessed_single_videos'
-    meomory_GB_int = 64
-    python_script_position = '/home/geuba03p/PyProjects/yolo_tools/movie_preprocessing/FrameSplitter.py'
-    user_name = 'geuba03p'
-    script_variables = f'--video_path {input_file_position} --output_folder {video_output_folder} --output_type videos'
-    gpu_num = 1
-    nodes = 1
-    cpus_per_task = 1
-    ntasks = 1
-
-    if partition == 'aoraki':
-        gpu_num = 0
-
-    script_parameters['partition'] =  partition
-    script_parameters['filename'] = slurm_filename
-    script_parameters['cpus_per_task'] = cpus_per_task
-    script_parameters['python_script'] = python_script_position
-    script_parameters['jobname'] =  f'split_{os.path.basename(input_file_position)}'
-    script_parameters['account_name'] = user_name
-    script_parameters['memory'] = meomory_GB_int
-    script_parameters['script_variables'] = script_variables
-    script_parameters['gpus_per_task'] = gpu_num
-    script_parameters['nodes'] = nodes
-    script_parameters['ntasks_per_node'] = ntasks
-    script_parameters['python_path'] = python_path
-
-    self.create_slurm_script(script_parameters)
-"""
