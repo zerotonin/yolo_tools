27d08c8b2c4603117f055914bb97008cf23b48a1
KoenvdGeest <koenvdgeest@hotmail.com>
2024-05-14 10:26:09 +1200

    jobnames in slurm canot have spaces

--- scripts/start_for_Koen.py
+++ scripts/start_for_Koen.py
@@ -3,14 +3,15 @@ import json
 
 # Example usage of the setup_experiments method
 if __name__ == "__main__":
-    # base_output_path = '/projects/sciences/zoology/geurten_lab/food_experiments/koen/analysis_folders/template_01'
-    # video_file_path = '/projects/sciences/zoology/geurten_lab/food_experiments/koen/new_vids'
-    base_output_path = '/home/geuba03p/food_example_video/output'
-    video_file_path = '/home/geuba03p/food_example_video/original/2024_03_28__16-19-28.mp4'
+    base_output_path = '/projects/sciences/zoology/geurten_lab/food_experiments/koen/analysis_folders/Column1Male_Agarose1p_Left_Agarose1p_Fructose26.2p_Right/'
+    video_file_path = '/projects/sciences/zoology/geurten_lab/food_experiments/koen/new_vids/2024_04_10__14-41-42_FirstcolumnMale_agarose1pLeft_agarose1pfructose26_2pRight.mp4'
+    
 
     # Load JSON data from file
     with open('config/path_config_local.json', 'r') as json_file:
         path_config = json.load(json_file)
+
+    print(path_config)
 
     experiment_setup = ExperimentSetupManager(base_output_path, path_config['db_file_path'], 
                                               video_file_path, path_config['python_interp'],
