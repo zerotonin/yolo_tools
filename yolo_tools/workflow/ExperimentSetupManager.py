84657a9d8612fd5099a4ec78ac0267556c10b8d6
bgeurten <bart.geurten@otago.ac.nz>
2024-05-31 14:09:27 +1200

    added the duration to the Slurm Manager

--- yolo_tools/workflow/ExperimentSetupManager.py
+++ yolo_tools/workflow/ExperimentSetupManager.py
@@ -322,10 +322,12 @@ class ExperimentSetupManager:
         self.meta_data_table.to_csv(self.file_manager.file_dict['meta_data_csv_file'],index=False)
 
     def run_full_work_flow(self, wait_on_process = None): 
-        self.slurm_job_manager = SlurmJobManager(self.file_manager,self.arena_info['arena_num'],self.meta_data_table,self.gpu_parttition)
+        self.slurm_job_manager = SlurmJobManager(self.file_manager,self.arena_info['arena_num'],self.meta_data_table,
+                                                 self.experiment_info['duration'],self.gpu_parttition)
         self.slurm_job_manager.manage_workflow(self.arena_info['arena_num'],wait_on_process)
 
     def rerun_trajectory_analysis(self,wait_on_process= None): 
-        self.slurm_job_manager = SlurmJobManager(self.file_manager,self.arena_info['arena_num'],self.meta_data_table,self.gpu_parttition)
+        self.slurm_job_manager = SlurmJobManager(self.file_manager,self.arena_info['arena_num'],self.meta_data_table,
+                                                 self.experiment_info['duration'],self.gpu_parttition)
         last_job_id = self.slurm_job_manager.rerun_traj_analysis(self.arena_info['arena_num'],wait_on_process)
         return last_job_id
