dff73a10b54ae486b7fa5c9aa8b65b3053a9ed0c
bgeurten <bart.geurten@otago.ac.nz>
2024-05-31 13:55:55 +1200

    added runtime for tracking

--- yolo_tools/workflow/SlurmJobManager.py
+++ yolo_tools/workflow/SlurmJobManager.py
@@ -105,6 +105,7 @@ class SlurmJobManager:
         script_parameters['nodes'] = nodes
         script_parameters['ntasks_per_node'] = ntasks
         script_parameters['module'] = 'detection'
+        script_parameters['runtime_sec'] = self.video_duration_sec*3
 
         self.create_slurm_script(script_parameters)
         return script_parameters['filename']
