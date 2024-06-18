8d8930c74330681d87dcd0dbfcfb7409d5a57954
bgeurten <bart.geurten@otago.ac.nz>
2024-06-18 14:35:44 +1200

    there has to be a cponda shell script to ensure that the write environment is used on the server

--- yolo_tools/workflow/SlurmJobManager.py
+++ yolo_tools/workflow/SlurmJobManager.py
@@ -83,6 +83,8 @@ class SlurmJobManager:
         content += f'#SBATCH --error={self.file_base_dir}/slurm_logs/%x.err\n'
         content += f'\n'
         content += f'sleep 5 # wait on auto mount\n'
+        content += f'source ~/conda.sh\n' # This should come from the filemanager
+        content += f'conda activate yolov8\n'
         content += f'{python_command}'
 
         # Write the SLURM script to a file
