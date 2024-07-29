dc4ac0ad26b80e92b78f918a0033fe5ca598b67c
bgeurten <bart.geurten@otago.ac.nz>
2024-07-29 15:04:37 +1200

    new slurm yolo train job

---
+++ scripts/slurm_train.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --job-name=clust_go1
+#SBATCH --account=geuba03p
+#SBATCH --partition=aoraki_gpu_H100,aoraki_gpu,aoraki_gpu_L40
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --gpus-per-task=1
+#SBATCH --time=04:00:00
+#SBATCH --mem=20GB
+
+# python -m yolo_tools.training_tools.train_bash_compatible  -d <dataset_dir> -i <images_dir> -l <labels_dir> -c <config_file> [-n <model_name>] [-e <epochs>] [--classes <class1> <class2> ...]
+dataset_dir='/projects/sciences/zoology/geurten_lab/AI_trainData/koen_3_chambers_circular/dataset'
+images_dir='/projects/sciences/zoology/geurten_lab/AI_trainData/koen_3_chambers_circular/frames'
+labels_dir='/projects/sciences/zoology/geurten_lab/AI_trainData/koen_3_chambers_circular/annotations'
+config_file='/projects/sciences/zoology/geurten_lab/AI_trainData/koen_3_chambers_circular/fly_arena_3Clin.yaml'
+model_name='fly_arena_3Clin'
+epochs=200
+#classes
+
+
+python -m yolo_tools.training_tools.train_bash_compatible -d $dataset_dir -i $images_dir -l $labels_dir -c $config_file -n $model_name -e $epochs
