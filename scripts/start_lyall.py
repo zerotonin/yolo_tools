c2dd22543c772b6a5b090f95bc6dd918de860d1e
bgeurten <bart.geurten@otago.ac.nz>
2024-09-30 12:23:53 +1300

    automatised the script further

--- scripts/start_lyall.py
+++ scripts/start_lyall.py
@@ -5,56 +5,62 @@ import os
 # Example usage of the setup_experiments method
 if __name__ == "__main__":
 
+    
     ###################
     # INPUT VARIABLES #
     ###################
 
-    folder_name  = '2024_09_04__12-21-38' # folder where this should be saved
-    mp4_filepath = '2024_09_04__12-21-38.mp4' # file name of the video file  you want to analyse
-    aoraki_gpu_partition = 'aoraki_gpu_L40' # 'aoraki_gpu' 'aoraki_gpu_L40' 'aoraki_gpu_H100'
+    for mp4_filepath in ['2024_09_12__09-30-00.mp4', '2024_09_23__09-15-00.mp4',
+                        '2024_09_13__09-40-13.mp4', '2024_09_27__09-30-00.mp4']:
+        # file name of the video file  you want to analyse
+        
 
-    ##############
-    # BASIC CODE # 
-    ##############
 
-    server_path = '/projects/sciences/zoology/geurten_lab/food_experiments/lyall'
-    base_output_path = f'{server_path}/analysis_folders/{folder_name}'
-    video_file_path = f'{server_path}/new_vids/{mp4_filepath}'
-    
-    # Make the folder
-    os.makedirs(base_output_path, exist_ok=True)
+        ##############
+        # BASIC CODE # 
+        ##############
 
-    # Load JSON data from file
-    with open('config/path_config_local.json', 'r') as json_file:
-        path_config = json.load(json_file)
+        folder_name  = mp4_filepath.split('.')[0] # folder where this should be saved
+        aoraki_gpu_partition = 'all_aoraki_gpus' # runs on all free gpu resources
 
-    print(path_config)
+        server_path = '/projects/sciences/zoology/geurten_lab/food_experiments/lyall'
+        base_output_path = f'{server_path}/analysis_folders/{folder_name}'
+        video_file_path = f'{server_path}/new_vids/{mp4_filepath}'
+        
+        # Make the folder
+        os.makedirs(base_output_path, exist_ok=True)
 
         # Load JSON data from file
+        with open('config/path_config_local.json', 'r') as json_file:
+            path_config = json.load(json_file)
 
-    with open('config/path_config_local.json', 'r') as json_file:
+        print(path_config)
 
-        path_config = json.load(json_file)
+            # Load JSON data from file
 
-    print(path_config)
+        with open('config/path_config_local.json', 'r') as json_file:
 
-    experiment_setup = ExperimentSetupManager(base_output_path, 
-                                              path_config['db_file_path'], 
-                                              video_file_path, 
-                                              path_config['python_interp'],
-                                              path_config['yolo_weights'],
-                                              path_config['conda_script_position'],
-                                              path_config['conda_env_name'], gpu_partition="aoraki_gpu_L40")
+            path_config = json.load(json_file)
 
-    experiment_setup.setup_experiments()
-    experiment_setup.display_experiment_overview()
-    experiment_setup.display_experiment_overview_arena_wise()
+        print(path_config)
 
-    if video_file_path:
-        experiment_setup.get_video_info()
-        experiment_setup.write_meta_data_table()
-        go = input("Do you want to run this job? (y/n)")
-        if go =='y':
-            experiment_setup.run_full_work_flow()
-        else:
-            print(f'you chose: {go}, therefore we did not start the jobs')
+        experiment_setup = ExperimentSetupManager(base_output_path, 
+                                                path_config['db_file_path'], 
+                                                video_file_path, 
+                                                path_config['python_interp'],
+                                                path_config['yolo_weights'],
+                                                path_config['conda_script_position'],
+                                                path_config['conda_env_name'], gpu_partition="aoraki_gpu_L40")
+
+        experiment_setup.setup_experiments()
+        experiment_setup.display_experiment_overview()
+        experiment_setup.display_experiment_overview_arena_wise()
+
+        if video_file_path:
+            experiment_setup.get_video_info()
+            experiment_setup.write_meta_data_table()
+            go = input("Do you want to run this job? (y/n)")
+            if go =='y':
+                experiment_setup.run_full_work_flow()
+            else:
+                print(f'you chose: {go}, therefore we did not start the jobs')
