6d77c772cf78cf7860c36c833e0ef8523e7775ec
Bart Geurten <bart.geurten@gmail.com>
2025-02-25 10:31:34 +1300

    testing

--- train_yolo11.sh
+++ train_yolo11.sh
@@ -5,7 +5,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --gpus-per-task=1
-#SBATCH --time=06:00:00
+#SBATCH --time=10:00:00
 #SBATCH --mem=20GB
 
 sleep 5  # wait on auto mount (if needed)
