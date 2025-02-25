88210a9959e3e833379dcd9578f21cf4eba081b3
bgeurten <bart.geurten@otago.ac.nz>
2025-02-25 20:23:15 +1300

    resumable faster rt_detr

--- train_rtdetr.sh
+++ train_rtdetr.sh
@@ -5,6 +5,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --gpus-per-task=1
+#SBATCH --cpus-per-task=16
 #SBATCH --time=10:00:00
 #SBATCH --mem=20GB
 
@@ -14,11 +15,13 @@ conda activate yolov8
 
 /home/geuba03p/miniconda3/envs/yolov8/bin/python /home/geuba03p/PyProjects/yolo_tools/single_ultralytics_train.py \
   --model rtdetr \
-  --pretrained rtdetr-l.pt \
+  --pretrained /home/geuba03p/object_det_comparison/rtdetr_run/weights/last.pt\
   --data /projects/sciences/zoology/geurten_lab/AI_trainData/uLytics_Lena/data.yaml \
   --epochs 400 \
-  --batch_size 3 \
+  --batch_size 8 \
   --imgsz 640 \
   --project /home/geuba03p/object_det_comparison \
   --name rtdetr_run \
+  --resume \
   --inference_images /projects/sciences/zoology/geurten_lab/AI_trainData/uLytics_Lena/images/val
+  
