f6169951aab667a91d204075dcc0d551e6adf261
bgeurten <bart.geurten@otago.ac.nz>
2026-01-26 12:28:00 +0100

    It is yolo11m not yolov11m

--- yolo_tools/training_tools/YoloWrapper.py
+++ yolo_tools/training_tools/YoloWrapper.py
@@ -29,7 +29,7 @@ class YoloWrapper:
         elif model_weights == 'medium':
             model_weights = 'yolov8m.pt'
         elif model_weights == '11medium':
-            model_weights = 'yolov11m.pt'
+            model_weights = 'yolo11m.pt'
         elif (not Path(model_weights).exists()) or (Path(model_weights).suffix != '.pt'):
             raise ValueError('The parameter model_weight should be "nano", "small" or a'
                              'path to a .pt file with saved weights')
